0ce24e837c7ad30c394deb00eea85971042ae3cf
Rahul Hegde <rhegde.hrahul@gmail.com>
2024-03-12 17:29:59 -0400

    finished mutually recursive helper functions

--- tinyL_Compiler/Compiler.py
+++ tinyL_Compiler/Compiler.py
@@ -20,9 +20,14 @@ def current_func_name(n: int = 0):
 # Note: I know global variables are bad practice in
 # general, but I'm doing this to mirror the scaffolding
 # given for the project in the C implementation
-regnum = 1
+regnum = 0
 content = None
 token, token_idx = None, None
+outfile = None
+
+# =========
+# Utilities
+# =========
 
 
 def error(msg: str):
@@ -34,6 +39,7 @@ def next_register():
     # regnum identifier
     global regnum
     regnum += 1
+    return regnum
 
 
 def is_digit(c: str):
@@ -62,12 +68,192 @@ def read_input(filepath):
 
 def next_token():
     global token, token_idx, content
-    if content == None or token == None or token_idx == None:
+    if content is None or token is None or token_idx is None:
         error("No content to parse, content is None")
     token_idx += 1
     token = content[token_idx]
 
-def code_gen(opcode: OpCode, field1: int, field2: int | None = None, field3: int | None = None):
 
+def code_gen(opcode: OpCode, field1, field2=None, field3=None):
     instr = Instruction(opcode, field1, field2, field3)
 
+    if outfile is None:
+        error("File Error: Outfile is None")
+        sys.exit(1)
+
+    print_instruction(outfile, instr)
+
+
+# ======================================================
+# Mutually Recursive Helpers for LL(1) Recursive Descent
+# ======================================================
+def digit():
+    global token
+    reg = next_register()
+    code_gen(OpCode.LOADI, reg, token)
+    next_token()
+    return reg
+
+
+def variable():
+    global token
+    reg = next_register()
+    code_gen(OpCode.LOAD, reg, token)
+    next_token()
+    return reg
+
+
+def expr():
+    global token
+    reg, left_reg, right_reg = None, None, None
+
+    if token == '+':
+        next_token()
+        left_reg = expr()
+        right_reg = expr()
+        reg = next_register()
+        code_gen(OpCode.ADD, reg, left_reg, right_reg)
+        return reg
+    elif token == '-':
+        next_token()
+        left_reg = expr()
+        right_reg = expr()
+        reg = next_register()
+        code_gen(OpCode.SUB, reg, left_reg, right_reg)
+        return reg
+    elif token == '*':
+        next_token()
+        left_reg = expr()
+        right_reg = expr()
+        reg = next_register()
+        code_gen(OpCode.MUL, reg, left_reg, right_reg)
+        return reg
+    elif token == '&':
+        next_token()
+        left_reg = expr()
+        right_reg = expr()
+        reg = next_register()
+        code_gen(OpCode.AND, reg, left_reg, right_reg)
+        return reg
+    elif token == '|':
+        next_token()
+        left_reg = expr()
+        right_reg = expr()
+        reg = next_register()
+        code_gen(OpCode.OR, reg, left_reg, right_reg)
+        return reg
+    elif is_digit(token):
+        return digit()
+    elif is_identifier(token):
+        return variable()
+    else:
+        error(f"Symbol {token} unknown")
+        sys.exit(1)
+
+
+def assign():
+    global token
+    identifier = token
+
+    next_token() # skip identifier
+
+    if token != '=':
+        error(f"Symbol {token} unknown")
+        sys.exit(1)
+    
+    next_token() # skip =
+
+    expr_result_reg = expr()
+    code_gen(OpCode.STORE, identifier, expr_result_reg)
+
+
+def read():
+    global token
+    next_token() # skip ?
+    identifier = token
+    code_gen(OpCode.READ, identifier)
+    next_token() # skip identifier
+
+
+def tinyL_print():
+    global token
+    next_token() # skip %
+    identifier = token
+    code_gen(OpCode.WRITE, identifier)
+    next_token() # skip identifier
+
+
+def stmt():
+    global token
+    if is_identifier(token):
+        assign()
+    elif token == '?':
+        read()
+    elif token == '%':
+        tinyL_print()
+
+
+def morestmts():
+    global token
+    if token == ';':
+        next_token()
+        stmtlist()
+    elif token == '!':
+        next_token()
+    else:
+        error(f"Program error.  Current input symbol is {token}")
+        sys.exit(1)
+
+
+def stmtlist():
+    global token
+    if is_identifier(token) or token in ['?', '%']:
+        stmt()
+        morestmts()
+    else:
+        error(f"Program error.  Current input symbol is {token}")
+        sys.exit(1)
+
+
+def program():
+    global token
+    if is_identifier(token) or token in ['?', '%']:
+        stmtlist()
+    else:
+        error(f"Program error.  Current input symbol is {token}")
+        sys.exit(1)
+
+    # if token != "!":
+    #     error(f"Program error.  Current input symbol is {token}")
+    #     sys.exit(1)
+
+
+# =============
+# Main function
+# =============
+def main() -> int:
+    # Populate global outfile path
+    global outfile
+    outfile = "tinyL.out"
+
+    print("------------------------------------------------")
+    print("CS314 compiler for tinyL")
+    print("------------------------------------------------")
+
+    if len(sys.argv) != 3:
+        sys.stderr.write("Use of command:\n  compile <tinyL file>\n")
+        sys.exit(1)
+
+    # Populate globals content and token
+    infile = sys.argv[2]
+    read_input(infile)
+
+    program()
+
+    print(f'\nCode written to file "{outfile}".\n')
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
